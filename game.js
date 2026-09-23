2d609fd4cb99a83053ec803641dcb9c92a6a840e
tjk <tuomo.kondelin@gmail.com>
2026-09-23 19:30:20 +0300

    javascript aloitettu

---
+++ game.js
@@ -0,0 +1,28 @@
+const CATEGORIES = [
+    {id:"maantieto", name:"Maantieto"},
+    {id:"yleistieto", name:"Yleistieto"},
+    {id:"historia", name:"Historia"},
+    {id:"kulttuuri", name:"Kulttuuri"},
+    {id:"tiede", name:"Tiede"},
+    {id:"urheilu", name:"Urheilu"},
+]
+
+const BOARD_SIZE = 18;
+
+const boardElement = document.querySelector("#board");
+
+function createBoardCoordinates(){
+    const coordinates = [];
+
+    //TODO koordinaattien täyttäminen
+
+
+    return coordinates;
+}
+
+function createBoard(){
+
+    const coordinates = createBoardCoordinates();
+
+    //TODO luodan kategoria elementit
+}
